fcea84c2218a7790f1867ec6a670734244e52a15
Karimjon0 <abidjonovkarimjon@gmail.com>
2024-09-08 22:57:52 +0500

    INFRA: Initialize Build Project

---
+++ Student.CRUD.Core.Ifrastructure.Build/Program.cs
@@ -0,0 +1,75 @@
+﻿
+
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Clients;
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "Student.CRUD Core Build Pipeline",
+
+    OnEvents = new Events
+    {
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "main" }
+        },
+
+        Push = new PushEvent
+        {
+            Branches = new string[] { "main" }
+        }
+    },
+
+
+    Jobs = new Dictionary<string, Job>
+    {
+        {
+            "build",
+            new Job
+            {
+                RunsOn = BuildMachines.WindowsLatest,
+
+                Steps = new List<GithubTask>
+                {
+                    new CheckoutTaskV2
+                    {
+                        Name = "Checking Out Code"
+                    },
+
+                    new SetupDotNetTaskV1
+                    {
+                        Name = "Setup .Net",
+
+                        TargetDotNetVersion = new TargetDotNetVersion
+                        {
+                            DotNetVersion = "8.0.204"
+                        }
+                    },
+
+                    new RestoreTask
+                    {
+                        Name = "Restoring Nuget Packages"
+                    },
+
+                    new DotNetBuildTask
+                    {
+                        Name = "Building Project"
+                    },
+
+                    new TestTask
+                    {
+                        Name = "Running Tests"
+                    }
+                }
+            }
+        }
+    }
+};
+
+var client = new ADotNetClient();
+
+client.SerializeAndWriteToFile(
+    adoPipeline: githubPipeline,
+    path: "../../../../.github/workflows/dotnet.yml");
